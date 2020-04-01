41e6b460dec78cde10e568201897771a8fafa2d3
kbennoune <kbennoune@gmail.com>
2020-04-01 12:40:37 -0400

    Parent model destruction should destroy children

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 class Device < ApplicationRecord
-  has_many :sensors
+  has_many :sensors, dependent: :destroy
   has_many :readings, through: :sensors
   has_many :alerts, through: :sensors
-  
+
   validates :serial_number, presence: true, uniqueness: true
 end
--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -1,8 +1,8 @@
 # A sensor record
 class Sensor < ApplicationRecord
   belongs_to :device
-  has_many :readings
-  has_one :alert
+  has_many :readings, dependent: :destroy
+  has_one :alert, dependent: :destroy
 
   validates :sensor_number, uniqueness: {scope: :device_id}
   

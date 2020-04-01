b3430d0ed490c6be996ded05f54155d9ca065e8d
kbennoune <kbennoune@gmail.com>
2020-04-01 10:48:05 -0400

    Style pages

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -5,4 +5,13 @@ module DevicesHelper
     onclick = "changeChart(\"#{type}\", \"chart-#{chart_id}\", \"#{data_url}\", #{chart_options.to_json}); event.preventDefault()"
     link_to(name, "#", html_options.merge(onclick: onclick))
   end
+
+  def dashboard_date(str)
+    begin
+      date = Date.parse(str)
+      date.to_s(:long)
+    rescue TypeError
+      ""
+    end
+  end
 end
